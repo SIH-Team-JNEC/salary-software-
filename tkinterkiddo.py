5d2068cc20858432a392fc7966b7eb9f076dae16
sharvi11s <sharvis4251csefy2@gmail.com>
2025-08-11 16:22:29 +0530

    added main.py

---
+++ tkinterkiddo.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+# 🪟 Make a window
+window = tk.Tk()
+window.title("My First GUI")
+
+# 🏷️ Add a label
+label = tk.Label(window, text="Hello! I'm a label!")
+label.pack()
+
+# 🔘 Add a button that changes the label
+def on_button_click():
+    label.config(text="You clicked the button!")
+
+button = tk.Button(window, text="Click me!", command=on_button_click)
+button.pack()
+
+# 🏁 Show the window
+window.mainloop()
