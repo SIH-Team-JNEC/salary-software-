d588c5c71caba20f652f7dee1f703c1e84f38830
Pegasis1002 <thewebserfer1234@gmail.com>
2025-08-14 10:51:44 +0530

    added a bisic database and functions

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import flet as ft
 from dashboard.dashboard_page import dashboard_page
+from database.database import add_user, get_users
+
+add_user('john_doe', 'john@example.com', "AB+ve")
+
+# Retrieve and print all users
+users = get_users()
+for user in users:
+    print(f'ID: {user[0]}, Username: {user[1]}, Email: {user[2]}, Blood group: {user[3]}')
+
 
 def main(page: ft.Page):
     page.title = "Simple Counter App"
