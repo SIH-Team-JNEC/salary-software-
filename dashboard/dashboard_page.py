4ce200c1bde4baa10dc25a4e85da2a374ba5c915
sayalijoshi1105 <sayalijoshi694@gmail.com>
2025-08-16 16:29:25 +0530

    resolved confictls #3

--- dashboard/dashboard_page.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def dashboard_page(page: ft.Page):
-    page.title = "Dashboard"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    dashboard_text = ft.Text("Welcome to the Dashboard!", size=48)
-
-    page.add(dashboard_text)
